275121646ff652eac0d3d2669cb7b6c3f643122f
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-09-03 19:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
+	let rn = prompt("Input number of rows");
+	let cn = prompt("Input number of columns");
+
+	let table = document.getElementById("myTable");
+	let r=0,c=0;
+	while(r==rn){
+		let tr = table.insertRow(r);
+		for(let i=0; i<cn; i++){
+		  tr.insertCell(i).innerHTML = "Row-"+r+" Column-"+i;
+		}
+		r++;
+	}
+	
   
 }
